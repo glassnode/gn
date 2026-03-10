25ce7e872db9f5c5f37d7c22cf2dc5371ce0a9a4
Ivaylo Ivanov <ivaylo.ivanov.ipi@gmail.com>
2026-03-10 09:54:32 +0200

    fix: build version

--- cmd/root.go
+++ cmd/root.go
@@ -7,12 +7,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var versionStr = "dev"
-
-func SetVersion(s string) {
-	versionStr = s
-}
-
 var rootCmd = &cobra.Command{
 	Use:   "gn",
 	Short: "Glassnode API command-line interface",
@@ -22,22 +16,26 @@ var rootCmd = &cobra.Command{
 	},
 }
 
-func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-}
-
 func init() {
+	rootCmd.Version = "dev"
 	rootCmd.PersistentFlags().String("api-key", "", "API key override")
 	rootCmd.PersistentFlags().StringP("output", "o", "json", "output format: json, csv, table")
 	rootCmd.PersistentFlags().Bool("dry-run", false, "print the request URL without executing")
 	rootCmd.PersistentFlags().String("timestamp-format", "humanized", "timestamp format: unix, humanized, or Go layout (e.g. 2006-01-02 15:04:05); applies to table/csv output")
-	rootCmd.Version = versionStr
 	rootCmd.SetVersionTemplate("gn version {{.Version}}\n")
 
 	rootCmd.AddCommand(configCmd)
 	rootCmd.AddCommand(metricCmd)
 	rootCmd.AddCommand(assetCmd)
+}
+
+func SetVersion(v string) {
+	rootCmd.Version = v
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
